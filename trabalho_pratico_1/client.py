f6c2591490e4de55eb617fa8e1b389ae3ad2de8b
Fábio Augusto <fabio.augusto1911@gmail.com>
2022-05-18 19:58:02 -0300

    Correction for sending replies to the server

--- trabalho_pratico_1/client.py
+++ trabalho_pratico_1/client.py
@@ -1,19 +1,25 @@
 from socket import *
 
+def getQuestions():
+    questions = ''
+    for i in range(0, 5, 1):
+        numAlternative = int(input(f"Informe o número de alternativas da questão {(i+1)}: "))
+        alternatives = ''
+        for j in range(0, numAlternative, 1):
+            value = input(f"Informe a resposta da alternativa {(j+1)}: ")
+            alternatives = alternatives + value
+        if (i < 4):
+            questions = questions + str(f"{i+1};{numAlternative};{alternatives}-")
+        else:
+            questions = questions + str(f"{i+1};{numAlternative};{alternatives}")
 
-def getQuestion():
-    numQuestion = int(input("Informe o número da questão: "))
-    numAlternative = int(input("Informe o número de alternativas: "))
-    response = input("Informe suas respostas: ")
-
-    return str(numQuestion) + ";" + str(numAlternative) + ";" + response
-
+    return questions
 
 serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
-sentence = getQuestion()
+sentence = getQuestions()
 clientSocket.send(sentence.encode())
 modifiedSentence = clientSocket.recv(1024)
 print('From Server:', modifiedSentence.decode())
--- trabalho_pratico_1/server.py
+++ trabalho_pratico_1/server.py
@@ -1,43 +1,18 @@
 from socket import *
-"""
-questionCount = [
-    {question: 1, error: 0, right: 0},
-    {question: 2, error: 0, right: 0},
-    {question: 3, error: 0, right: 0},
-    {question: 4, error: 0, right: 0},
-    {question: 5, error: 0, right: 0},
-]
 
 FEEDBACK = [
-    {question: 1, quantity: 4, response: 'VFVF'},
-    {question: 2, quantity: 5, response: 'VVVFF'},
-    {question: 3, quantity: 2, response: 'FF'},
-    {question: 4, quantity: 3, response: 'VFV'},
-    {question: 5, quantity: 3, response: 'FVF'},
+    '1;4;VFVF',
+    '2;5;VVVFF',
+    '3;2;FF',
+    '4;3;VFV',
+    '5;3;FVF',
 ]
-"""
-
 
 def resolveQuestion(data):
-    """
-    response = data.split(";")
-
-    responseFinal = ''
-
-    for ask in FEEDBACK:
-        if(response[0] == ask.question):
-            if(response[1] == ask.quantity):
-                sumNote = 0
-                for i in (0, ask.quantity, 1):
-                    if (response[2][i] == ask.response[i]):
-                        sumNote = sumNote + 1
-            else:
-                responseFinal = 'Número de alternativas errada'
-        else:
-            responseFinal = 'Resposta não encontrada'
-    """
-    return data.replace(";", " oi luana ")
-
+    question = data.split('-')
+    print(question)
+    print(data)
+    return "Connection OK!"
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
