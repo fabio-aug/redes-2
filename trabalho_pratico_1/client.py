48e670a3d59756d8b82d462978706fbe8238a0be
Fábio Augusto <fabio.augusto1911@gmail.com>
2022-05-31 09:59:24 -0300

    Add comments

--- trabalho_pratico_1/client.py
+++ trabalho_pratico_1/client.py
@@ -1,27 +1,46 @@
 from socket import *
 from urllib import response
 
+
+# Função responsável por pegar as respostas do aluno;
 def getQuestions():
+    # Array para guardar as respostas do aluno;
     questions = []
 
+    # Estrutura para pegar as 5 questões;
+    # Pergunta o número de alternativas;
+    # Logo em seguida pega as respostas de acordo com o número de alternativas;
     for i in range(0, 5, 1):
-        numAlternative = int(input(f"Informe o número de alternativas da questão {(i+1)}: "))
+        numAlternative = int(
+            input(f"Informe o número de alternativas da questão {(i+1)}: "))
         alternatives = ''
         for j in range(0, numAlternative, 1):
             value = input(f"Informe a resposta da alternativa {(j+1)}: ")
             alternatives = alternatives + value
         questions.append(str(f"{i+1};{numAlternative};{alternatives}"))
+
+    # Antes de enviar, transforma o array de respostas em uma string e joga tudo em maiúsculo;
     return str(questions).upper()
 
+
+# Função mostrar o resultado do aluno recebido do servidor;
 def showResult(data):
+    # Transforma a string em uma expressão python, nesse caso, um dicionário enviado pelo servidor;
     responseData = eval(data)
 
+    # Percorre todas as respostas corrigidas pelo servidor;
+    # Forma uma string só, quebrando linha para cada respostas;
+    # No fim junta com a pontuação final;
     printUser = '\nGabarito do usuário: \n'
-    for responseQuestion in responseData['user']:
+    for responseQuestion in responseData['questionScore']:
         result = responseQuestion.split(";")
-        printUser = printUser + str(f"Questão: {result[0]} - Acerto: {result[1]} - Erro: {result[2]} \n")
-    printUser = printUser + responseData['grade']
+        printUser = printUser + \
+            str(f"Questão: {result[0]} - Acerto: {result[1]} - Erro: {result[2]} \n")
+    printUser = printUser + responseData['finalGrade']
+
+    # Mostra as respostos corrigidas e a nota final;
     print(printUser)
+
 
 serverName = 'localhost'
 serverPort = 12000
--- trabalho_pratico_1/server.py
+++ trabalho_pratico_1/server.py
@@ -1,5 +1,7 @@
 from socket import *
 
+# Gabarito contendo todas as respostas;
+# Por padrão foram definidas 5 questões;
 FEEDBACK = [
     '1;4;VFVF',
     '2;5;VVVFF',
@@ -8,16 +10,24 @@ FEEDBACK = [
     '5;3;FVF',
 ]
 
+# Pontuação global de acertos das questões;
 PONCTUATION_SUCCESS = [0] * 5
+# Pontuação global de erros das questões;
 PONCTUATION_ERROR = [0] * 5
 
+
+# Função responsável por avaliar as respostas do aluno e calcular o resultado final;
 def resolveQuestion(data):
+    # Transforma a string em uma expressão python, nesse caso, um array enviado pelo aluno;
     questions = eval(data)
 
     userPonctuation = []
     userGrade = 0
 
-    for i in range(0,5,1):
+    # Percorrendo todas as questões do gabarito e respostas enviadas pelo aluno;
+    # Avaliamos TODA a questão, ou seja, se errar UMA alternativa consideramos a questão como ERRADA;
+    # De acordo que o aluno acerta e erra as questões adicionamos as respostas ao array de correção 'userPonctuation';
+    for i in range(0, 5, 1):
         if (questions[i] == FEEDBACK[i]):
             userPonctuation.append(str(f"{i+1};1;0"))
             PONCTUATION_SUCCESS[i] += 1
@@ -26,14 +36,19 @@ def resolveQuestion(data):
             userPonctuation.append(str(f"{i+1};0;1"))
             PONCTUATION_ERROR[i] += 1
 
-        print(str(f"Questão: {i+1} - Acerto: {PONCTUATION_SUCCESS[i]} - Erro: {PONCTUATION_ERROR[i]}"))
+        # Mostra o resultado para o professor no servidor;
+        print(str(
+            f"Questão: {i+1} - Acerto: {PONCTUATION_SUCCESS[i]} - Erro: {PONCTUATION_ERROR[i]}"))
 
-        responseData = {
-            'user': userPonctuation,
-            'grade' : str(f"Nota: {userGrade}/5")
-        }
+    # Cria um dicionário com o resultado final (array com o resultado de cada questão e a soma total da pontuação do aluno);
+    # Logo em seguida transformamos em uma string para o envio;
+    responseData = {
+        'questionScore': userPonctuation,
+        'finalGrade': str(f"Nota: {userGrade}/5")
+    }
 
     return str(responseData)
+
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
